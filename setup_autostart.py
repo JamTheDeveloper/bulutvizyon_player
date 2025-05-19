48790d0cc2d3b97eb47927263fd23c3a8df8435e
CEM SİPAHİ <120724435+JamTheDeveloper@users.noreply.github.com>
2025-05-19 17:12:14 +0300

    Update setup_autostart.py

--- setup_autostart.py
+++ setup_autostart.py
@@ -1,9 +1,13 @@
 import os
+import shutil
+import glob
 
 # API kodunu kaydedeceğimiz dosyanın yolu
 API_CODE_FILE = os.path.expanduser("~/.config/chromium_api_code.txt")
 AUTOSTART_DIR = os.path.expanduser("~/.config/autostart")
 AUTOSTART_FILE = os.path.join(AUTOSTART_DIR, "chromium_autostart.desktop")
+SPLASH_IMAGE = "/usr/share/plymouth/themes/ubuntu-logo/ubuntu-logo.png"
+BACKUP_SPLASH = "/usr/share/plymouth/themes/ubuntu-logo/ubuntu-logo.png.backup"
 
 
 def save_api_code(api_code):
@@ -25,7 +29,7 @@ def read_api_code():
 def create_autostart_file(api_code):
     """Chromium'u başlatmak için autostart dosyasını oluşturur."""
     # Chromium parametreleri
-    url = "http://168.119.57.127:5005/preview/{}".format(api_code)
+    url = "https://bulutvizyon.com/viewer/{}".format(api_code)
     chromium_command = (
         "chromium-browser --ignore-certificate-errors --disable-web-security "
         "--kiosk --disable-infobars --disable-session-crashed-bubble {}".format(url)
@@ -47,6 +51,27 @@ Name=Chromium Auto Start
     print("Autostart dosyası oluşturuldu: {}".format(AUTOSTART_FILE))
 
 
+def change_splash_screen(new_splash_path):
+    """Açılış logosunu değiştirir."""
+    if os.path.exists(SPLASH_IMAGE):
+        # Yedek al
+        if not os.path.exists(BACKUP_SPLASH):
+            shutil.copy2(SPLASH_IMAGE, BACKUP_SPLASH)
+        # Yeni logoyu kopyala
+        shutil.copy2(new_splash_path, SPLASH_IMAGE)
+        print("Açılış logosu değiştirildi.")
+    else:
+        print("Hata: Ubuntu logo dosyası bulunamadı.")
+
+
+def find_logo_file():
+    """Klasördeki PNG dosyasını bulur."""
+    png_files = glob.glob("*.png")
+    if png_files:
+        return os.path.abspath(png_files[0])
+    return None
+
+
 def main():
     # Daha önce kaydedilmiş bir API kodu var mı kontrol et
     api_code = read_api_code()
@@ -57,6 +82,14 @@ def main():
 
     # Autostart dosyasını oluştur
     create_autostart_file(api_code)
+
+    # Açılış logosunu değiştir
+    logo_path = find_logo_file()
+    if logo_path:
+        print(f"Bulunan logo: {logo_path}")
+        change_splash_screen(logo_path)
+    else:
+        print("Klasörde PNG dosyası bulunamadı.")
 
     print("Sistem açılışında Chromium otomatik olarak başlayacaktır.")
 
